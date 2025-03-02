13969bd6c132ca03c7e0deb674b2484ae05e04c9
koki.matsumoto <matsumoto.k.bj@m.titech.ac.jp>
2025-03-03 00:08:26 +0900

    ヘッダのみ作成

--- cpp/inc/flow_field.hpp
+++ cpp/inc/flow_field.hpp
@@ -2,7 +2,29 @@
 #define FIELD_H
 
 #include <vector>
+#include "mesh2d.hpp"
 
+class FlowField {
+    private:
+        int nx, ny;
+        std::vector<std::vector<double>> u, v, p;
+    
+    public:
+        FlowField(const Mesh2D& mesh);
+
+        void update(
+            const std::vector<std::vector<double>>& du,
+            const std::vector<std::vector<double>>& dv,
+            const std::vector<std::vector<double>>& dp
+        );
+
+        const std::vector<std::vector<double>>& get_u() const;
+        const std::vector<std::vector<double>>& get_v() const;
+        const std::vector<std::vector<double>>& get_p() const;
+
+        //for debug
+        void print() const;
+};
 
 
 
